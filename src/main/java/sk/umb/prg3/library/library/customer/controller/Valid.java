ed6d1c4e4394a94f7de6c33241c65e40bf0d9d1c
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2023-03-29 20:41:16 +0200

    bookcontroller

---
+++ src/main/java/sk/umb/prg3/library/library/customer/controller/Valid.java
@@ -0,0 +1,5 @@
+package sk.umb.prg3.library.library.customer.controller;
+
+public @interface Valid {
+
+}
---
+++ src/main/java/sk/umb/prg3/library/library/customer/persistence/entity/Book.java
@@ -0,0 +1,40 @@
+package sk.umb.prg3.library.library.customer.persistence.entity;
+
+public class Book {
+
+    public Object getName() {
+        return null;
+    }
+
+    public void setName(Object name) {
+    }
+
+    public Object getIsbn() {
+        return null;
+    }
+
+    public void setIsbn(Object isbn) {
+    }
+
+    public Object getAuthorFirstName() {
+        return null;
+    }
+
+    public Object getAuthorLastName() {
+        return null;
+    }
+
+    public void setAuthorFirstName(Object authorFirstName) {
+    }
+
+    public void setAuthorLastName(Object authorLastName) {
+    }
+
+    public Object getBookCount() {
+        return null;
+    }
+
+    public void setBookCount(Object bookCount) {
+    }
+
+}
