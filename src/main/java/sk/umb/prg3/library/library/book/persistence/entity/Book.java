65f345d56d692aad5e73796bb6672abb991cbfbd
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2023-04-06 08:31:50 +0200

    Fixed error in bookcontroller and updated book entity

--- src/main/java/sk/umb/prg3/library/library/book/persistence/entity/Book.java
+++ src/main/java/sk/umb/prg3/library/library/book/persistence/entity/Book.java
@@ -1,40 +1,69 @@
 package sk.umb.prg3.library.library.book.persistence.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 
-    public Object getName() {
-        return null;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String isbn;
+    private String authorFirstName;
+    private String authorLastName;
+    private Integer bookCount;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
     }
 
-    public void setName(Object name) {
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public Object getIsbn() {
-        return null;
+    public String getName() {
+        return name;
     }
 
-    public void setIsbn(Object isbn) {
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Object getAuthorFirstName() {
-        return null;
+    public String getIsbn() {
+        return isbn;
     }
 
-    public Object getAuthorLastName() {
-        return null;
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 
-    public void setAuthorFirstName(Object authorFirstName) {
+    public String getAuthorFirstName() {
+        return authorFirstName;
     }
 
-    public void setAuthorLastName(Object authorLastName) {
+    public void setAuthorFirstName(String authorFirstName) {
+        this.authorFirstName = authorFirstName;
     }
 
-    public Object getBookCount() {
-        return null;
+    public String getAuthorLastName() {
+        return authorLastName;
     }
 
-    public void setBookCount(Object bookCount) {
+    public void setAuthorLastName(String authorLastName) {
+        this.authorLastName = authorLastName;
     }
 
+    public Integer getBookCount() {
+        return bookCount;
+    }
+
+    public void setBookCount(Integer bookCount) {
+        this.bookCount = bookCount;
+    }
 }
